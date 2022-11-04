3a1520903572512eeac34daada3c50eba735ab29
dunowljj <uuufg3322@gmail.com>
2022-11-04 18:37:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/jakspringbootservice/JakSpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakspringbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakSpringbootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakSpringbootServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jakspringbootservice/JakSpringbootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jakspringbootservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakSpringbootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
