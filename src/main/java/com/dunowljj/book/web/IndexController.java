e3829073eb405133f94668237aa24f6098d36c96
dunowljj <uuufg3322@gmail.com>
2022-11-17 18:31:14 +0900

    feat : [freelec 5.3장 완료] Oauth 2.0 로그인 기능 구현

--- src/main/java/com/dunowljj/book/web/IndexController.java
+++ src/main/java/com/dunowljj/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dunowljj.book.web;
 
+import com.dunowljj.book.config.auth.SessionUser;
 import com.dunowljj.book.service.posts.PostsService;
 import com.dunowljj.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
