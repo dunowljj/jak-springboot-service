74f0a71caba7129bf5163216a0fbc20f43eaf1b6
dunowljj <uuufg3322@gmail.com>
2022-11-24 22:32:57 +0900

    feat : 행사 페이지 view 추가(폼 제외) 및 posts 패키지 잘못된 위치 수정

--- src/main/java/com/dunowljj/book/web/events/EventsController.java
+++ src/main/java/com/dunowljj/book/web/events/EventsController.java
@@ -1,7 +1,41 @@
 package com.dunowljj.book.web.events;
 
+import com.dunowljj.book.config.auth.LoginUser;
+import com.dunowljj.book.config.auth.SessionUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EventsController {
+
+    @GetMapping("/events")
+    public String home(Model model,  @LoginUser SessionUser user) {
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "events/home";
+    }
+
+    @GetMapping("/events/reservation")
+    public String reservation(Model model,  @LoginUser SessionUser user) {
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "events/reservation";
+    }
+
+    @GetMapping("/events/registration")
+    public String registration(Model model,  @LoginUser SessionUser user) {
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "events/registration";
+    }
 }
