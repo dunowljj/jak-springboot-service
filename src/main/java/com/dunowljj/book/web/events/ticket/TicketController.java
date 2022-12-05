a3f31a018fe44d5e41ef6deca54202f280d5b952
dunowljj <uuufg3322@gmail.com>
2022-12-05 23:30:33 +0900

    [ISSUE-8] 티켓-행사 목록 및 세부사항 확인 기능 구현

---
+++ src/main/java/com/dunowljj/book/web/events/ticket/TicketController.java
@@ -0,0 +1,30 @@
+package com.dunowljj.book.web.events.ticket;
+
+import com.dunowljj.book.service.events.event.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/events")
+
+public class TicketController {
+
+    private final EventService eventService;
+
+    @GetMapping("/ticket/list")
+    public String save(Model model) {
+        model.addAttribute("eventList", eventService.findAllDESC());
+        return "/events/ticket/ticket-list";
+    }
+
+    @GetMapping("/ticket/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("event", eventService.findById(id));
+        return "/events/ticket/ticket-detail";
+    }
+}
