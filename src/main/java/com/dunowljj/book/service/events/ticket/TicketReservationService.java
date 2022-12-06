9980f2619cfd706c0a58774afa5f04631dda1caf
dunowljj <uuufg3322@gmail.com>
2022-12-06 23:32:09 +0900

    [ISSUE-8] 티켓-행사 예약 기능 구현(#8)

---
+++ src/main/java/com/dunowljj/book/service/events/ticket/TicketReservationService.java
@@ -0,0 +1,32 @@
+package com.dunowljj.book.service.events.ticket;
+
+import com.dunowljj.book.domain.events.event.Event;
+import com.dunowljj.book.domain.events.event.EventRegistration;
+import com.dunowljj.book.domain.events.event.EventRegistrationRepository;
+import com.dunowljj.book.domain.events.event.EventRepository;
+import com.dunowljj.book.domain.events.ticket.TicketReservationRepository;
+import com.dunowljj.book.web.dto.events.ticket.TicketReserveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class TicketReservationService {
+
+    private final TicketReservationRepository reservationRepository;
+    private final EventRegistrationRepository eventRegistrationRepository;
+    private final EventRepository eventRepository;
+
+    @Transactional
+    public Long reserve(TicketReserveRequestDto requestDto) {
+        Long eventId = requestDto.getEventId();
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 행사가 존재하지 않습니다. id="+ eventId));
+
+        EventRegistration registration = eventRegistrationRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 행사등록정보가 존재하지 않습니다. id=" + eventId));
+
+       return reservationRepository.save(requestDto.toEntity(event, registration.getMember())).getId();
+    }
+}
